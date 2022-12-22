eea86ab21e543542344617b32664e4598905d43a
Marcel Mueller <63649718+marcelmbn@users.noreply.github.com>
2022-12-22 15:25:36 +0100

    Initial read-in accomplished

--- evaluate.py
+++ evaluate.py
@@ -1,10 +1,18 @@
 #!/bin/python3
 
 import sys
+import periodictable as pt
 
-def take_second(elem):
-    return elem[1]
+# create a dictionary of the elements
+pesdict={"symb":[],"numb":[],"startline":[],"endline":[],"nbf":[],"npr":[]}
+for el in pt.elements:
+    if el.number == 0:
+        continue
+    pesdict["symb"].append(el.symbol)
+    pesdict["numb"].append(el.number)
+print(pesdict["symb"],pesdict["numb"])
 
+# check if the input argument is given
 try:
     str(sys.argv[1])
 except:
@@ -16,29 +24,57 @@ except:
     print("Could not open file "+str(sys.argv[1])+".")
     exit()
 
-autokcal=627.509
+# read the file
 lines = f.readlines()
-
-LCCSDT = {}
-
+k=0
 for i in range(0,len(lines)):
-    if "CONF" in lines[i].strip():
-        LCCSDT[lines[i].strip()] = float(lines[i+1].split()[3])
+    if "NewGTO" in lines[i].strip():
+        pesdict["startline"].append(i+1)
+    if "end" in lines[i].strip():
+        pesdict["endline"].append(i)
+        # print start and end line
+        print(pesdict["startline"][k],pesdict["endline"][k])
+        k=k+1
 
-compound=sys.argv[1].split(".")[0]
-LCCSDT=sorted(LCCSDT.items(), key=take_second)
-DFT = {}
-for CONF in LCCSDT:
-    dft_file=compound+"/"+CONF[0]+"/ridft.out"
-    try:
-        f2=open(dft_file,"r")
-    except:
-        print("Could not find DFT calculation for conformer: "+CONF[0])
-        sys.exit()
-    lines=f2.readlines()
-    DFT[CONF[0]]=float(lines[[i for i, s in enumerate(lines) if "total energy" in s][1]].split()[4])
-
-lowest=LCCSDT[0]
-for CONF in LCCSDT:
-    #print(CONF[0]+" "+str((CONF[1]-lowest[1])*autokcal)+" "+str((DFT[CONF[0]]-DFT[lowest[0]])*autokcal))
-    print(str((DFT[CONF[0]]-DFT[lowest[0]])*autokcal))
+k=0
+exponents=[]
+coefficients=[]
+# go through all entries in the dictionary and read the basis functions
+for i in range(0,len(pesdict["symb"])):
+    exptmp=[]
+    coefftmp=[]
+    for j in range(pesdict["startline"][i],pesdict["endline"][i]):
+        pesdict["nbf"].append(0)
+        pesdict["npr"].append(0)
+        if "S" in lines[j].strip():
+            pesdict["nbf"][k]=pesdict["nbf"][k]+1
+            nprs=int(lines[j].strip().split()[1])
+            pesdict["npr"][k]=pesdict["npr"][k]+nprs
+            for m in range(j+1,j+nprs+1):
+                exptmp.append(lines[m].strip().split()[1])
+                coefftmp.append(lines[m].strip().split()[2])
+            print(coefftmp)
+        if "P" in lines[j].strip():
+            pesdict["nbf"][k]=pesdict["nbf"][k]+1
+            nprs=int(lines[j].strip().split()[1])
+            pesdict["npr"][k]=pesdict["npr"][k]+nprs
+            for m in range(j+1,j+nprs+1):
+                exptmp.append(lines[m].strip().split()[1])
+                coefftmp.append(lines[m].strip().split()[2])
+            print(coefftmp)
+        if "D" in lines[j].strip():
+            pesdict["nbf"][k]=pesdict["nbf"][k]+1
+            nprs=int(lines[j].strip().split()[1])
+            pesdict["npr"][k]=pesdict["npr"][k]+nprs
+            for m in range(j+1,j+nprs+1):
+                exptmp.append(lines[j+m].strip().split()[1])
+                coefftmp.append(lines[j+m].strip().split()[2])
+            print(coefftmp)
+        print(lines[j].strip())
+    print(pesdict["symb"][i],pesdict["numb"][i],pesdict["nbf"][k],pesdict["npr"][k])
+    exponents.append(exptmp)
+    coefficients.append(coefftmp)
+    print(exponents[k])
+    print(coefficients[k])
+    k=k+1
+    exit()
