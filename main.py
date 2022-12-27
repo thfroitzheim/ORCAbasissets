71fff0404a1a24ee2cabd106df64a9ac6cc9d35f
Marcel Mueller <marcel.mueller@thch.uni-bonn.de>
2022-12-27 21:16:49 +0100

    built module for ORCA format read-in and made code nicer

---
+++ main.py
@@ -0,0 +1,78 @@
+#!/bin/python3
+
+import sys
+from src.readin import orcaformat
+
+def print_help():
+    print("Usage: python3 evaluate.py <input file>")
+    print("Example: python3 evaluate.py input.gjf")
+    print("This script reads a Gaussian input file and extracts the basis set.")
+    print("The output is a dictionary with the following keys and values:")
+    print("symb: list of the symbols of the elements")
+    print("numb: list of the atomic numbers of the elements")
+    print("startline: list of the line numbers where the basis functions start")
+    print("endline: list of the line numbers where the basis functions end")
+    print("nbf: list of the number of basis functions per element")
+    print("npr: list of the number of primitives per element")
+    print("lnpr: list of the number of primitives per basis function")
+    print("angmom: list of the angular momentum of the basis functions")
+    print("exponents: list of the exponents of the primitives")
+    print("coefficients: list of the coefficients of the primitives")
+    print("The dictionary is called pesdict.")
+    print("The basis functions are stored in a list of dictionaries called bf.")
+    print("The dictionary bf contains the keys lnpr and angmom.")
+    print("The list of exponents and coefficients is called exponents and coefficients.")
+
+# check if the input argument is given
+try:
+    str(sys.argv[1])
+except:
+    print("No input argument.")
+    exit()
+try:
+    file = open(str(sys.argv[1]))
+    print("File "+str(sys.argv[1])+" opened.")
+except:
+    print("Could not open file "+str(sys.argv[1])+".")
+    exit()
+
+try:
+    str(sys.argv[2])
+    if str(sys.argv[2]) == "-h":
+        print_help()
+        exit()
+    elif str(sys.argv[2]) == "--help":
+        print_help()
+        exit()
+    else:
+        print("The last element is: "+str(sys.argv[2]))
+        desiredelem = int(str(sys.argv[2]))
+except:
+    print("No second input argument.")
+    desiredelem = 86
+
+print(desiredelem)
+
+# read the file
+# basis is a dictionary with the following keys and values:
+# symb: list of the symbols of the elements
+# numb: list of the atomic numbers of the element
+basis = orcaformat(file)
+
+# print only the full basis function for the desired element
+print("The number basis functions for the desired element is:")
+print(basis["nbf"][desiredelem-1])
+print("The number of primitives for the desired element is:")
+print(basis["npr"][desiredelem-1])
+print("The basis functions are:")
+k=0
+for i in range(0,basis["nbf"][desiredelem-1]):
+    print("Basis function "+str(i+1)+":")
+    print("  Angular momentum: "+basis["angmom"][desiredelem-1][i])
+    print("  Number of primitives: "+str(basis["lnpr"][desiredelem-1][i]))
+    print("  Exponents and coefficients:")
+    for j in range(0,basis["lnpr"][desiredelem-1][i]):
+        print(j+1)
+        print("    "+str(basis["exponents"][desiredelem-1][k]))
+        print("    "+str(basis["coefficients"][desiredelem-1][k]))
+        k+=1
---
+++ src/readin.py
@@ -0,0 +1,90 @@
+#!/bin/python3
+
+# This script reads a Gaussian input file and extracts the basis set.
+import periodictable as pt
+
+def orcaformat(f):
+    # create a dictionary of the elements
+    pesdict={"symb":[],"numb":[],"startline":[],"endline":[],"nbf":[],"npr":[]}
+    for el in pt.elements:
+        if el.number == 0:
+            continue
+        elif el.number >= 87:
+            break
+        pesdict["symb"].append(el.symbol)
+        pesdict["numb"].append(el.number)
+
+    # read the file
+    lines = f.readlines()
+    for i in range(0,len(lines)):
+        if "NewGTO" in lines[i].strip():
+            pesdict["startline"].append(i+1)
+        if "end" in lines[i].strip():
+            pesdict["endline"].append(i)
+
+    bf = []
+    exponents=[]
+    coefficients=[]
+    # create a dictionary with element symbol, number, nbf, npr, list[angular momenta],
+    #   list[exponents], list[coefficients]  
+    basisset = {"symb":[],"numb":[],"nbf":[],"npr":[],"angmom":[],"lnpr":[],"exponents":[],"coefficients":[]}
+    # go through all entries in the dictionary and read the basis functions
+    for i in range(0,len(pesdict["symb"])):
+        l=0
+        exptmp=[]
+        coefftmp=[]
+        bfdict={"lnpr":[],"angmom":[]} # dictionary for the basis functions
+        # set number of basis functions and primitives to zero for each element
+        pesdict["nbf"].append(0)
+        pesdict["npr"].append(0)
+        for j in range(pesdict["startline"][i],pesdict["endline"][i]):
+            if "S" in lines[j].strip():
+                bfdict["angmom"].append("S")
+            elif "P" in lines[j].strip():
+                bfdict["angmom"].append("P")
+            elif "D" in lines[j].strip():
+                bfdict["angmom"].append("D")
+            elif "F" in lines[j].strip():
+                bfdict["angmom"].append("F")
+            elif "G" in lines[j].strip():
+                print("No support of F or higher functions.")
+                exit()
+            elif "end" in lines[j].strip():
+                break
+            else:
+                continue
+            num = int(lines[j].strip().split()[1])
+            bfdict["lnpr"].append(num)
+            pesdict["nbf"][i]=pesdict["nbf"][i]+1
+            pesdict["npr"][i]=pesdict["npr"][i]+bfdict["lnpr"][l]
+            for m in range(j+1,j+bfdict["lnpr"][l]+1):
+                exptmp.append(lines[m].strip().split()[1])
+                coefftmp.append(lines[m].strip().split()[2])
+            l+=1
+
+        # Development printouts
+        # print(pesdict["symb"][i],pesdict["numb"][i],pesdict["nbf"][i],pesdict["npr"][i])
+        #######################
+        exponents.append(exptmp)
+        coefficients.append(coefftmp)
+        bf.append(bfdict)
+
+        # Development printouts
+        # print(bf[i])
+        # print(exponents[i])
+        # print(coefficients[i])
+        #######################
+        
+        # fill the dictionary with the basis set information
+        basisset["symb"].append(pesdict["symb"][i])
+        basisset["numb"].append(pesdict["numb"][i])
+        basisset["nbf"].append(pesdict["nbf"][i])
+        basisset["npr"].append(pesdict["npr"][i])
+        basisset["angmom"].append(bf[i]["angmom"])
+        basisset["lnpr"].append(bf[i]["lnpr"])
+        basisset["exponents"].append(exptmp)
+        basisset["coefficients"].append(coefftmp)
+
+        #end of iteration
+
+    return basisset
